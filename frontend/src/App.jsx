606ea98fd5f5ab8b2762bd8a24ca998008e9eaae
taha_ouad <tahaouad04@gmail.com>
2025-02-01 01:50:05 +0100

    Initial commit: Setup project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">
+        Test to check project enveronment
+      </h1>
+    </div>
+  );
+}
+
+
+export default App
